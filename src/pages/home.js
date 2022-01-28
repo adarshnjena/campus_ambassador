6d3841e890bbc1b67ccdd79912b25ed9c2f00c65
adarshnjena <adarshnjena@gmail.com>
2022-01-28 15:38:40 +0530

    searchbar removed

--- src/pages/home.js
+++ src/pages/home.js
@@ -50,12 +50,12 @@ function Home({ close, setClose, setNavbarVisible }) {
               Good morning,
               <span> Adarsh Jena</span>
             </Text>
-            <InputContainer>
+            {/* <InputContainer>
               <Icon>
                 <FiSearch />
               </Icon>
               <Input type="text" placeholder="Search for projects" />
-            </InputContainer>
+            </InputContainer> */}
           </ColumnTwo>
           <ColumnOne>
             <ColumnThree>
@@ -138,34 +138,34 @@ const ColumnThree = styled.div`
     align-items: center;
   }
 `;
-const InputContainer = styled.div`
-  display: flex;
-`;
+// const InputContainer = styled.div`
+//   display: flex;
+// `;
 
-const Icon = styled.div`
-  height: 3rem;
-  width: 3rem;
-  background-color: #dce4ff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-top-left-radius: 0.5rem;
-  border-bottom-left-radius: 0.5rem;
-  svg {
-    color: #555555;
-  }
-`;
-const Input = styled.input`
-  border: none;
-  background-color: #dce4ff;
-  border-top-right-radius: 0.5rem;
-  border-bottom-right-radius: 0.5rem;
-  color: #464646;
+// const Icon = styled.div`
+//   height: 3rem;
+//   width: 3rem;
+//   background-color: #dce4ff;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-top-left-radius: 0.5rem;
+//   border-bottom-left-radius: 0.5rem;
+//   svg {
+//     color: #555555;
+//   }
+// `;
+// const Input = styled.input`
+//   border: none;
+//   background-color: #dce4ff;
+//   border-top-right-radius: 0.5rem;
+//   border-bottom-right-radius: 0.5rem;
+//   color: #464646;
 
-  &:focus {
-    border: none;
-    outline: none;
-  }
-`;
+//   &:focus {
+//     border: none;
+//     outline: none;
+//   }
+// `;
 
 export default Home;
